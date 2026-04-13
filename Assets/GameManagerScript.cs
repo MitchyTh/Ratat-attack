e585f30eb2727f4ae3dcf0f0a2892a3b7a2b5fb3
Jackson P <user@FIH-COM23a103.fsu.edu>
2026-04-13 16:27:38 -0400

    Add working fridge

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public bool gameStarted = false;
+    public bool roundOver = false;
+    public int playerScore = 0;
+    public float roundTime = 420f;
+    public float timeRemaining = 420f;
+    public static GameManagerScript Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameStarted && !roundOver)
+        {
+            timeRemaining -= Time.deltaTime;
+            if (timeRemaining <= 0)
+            {
+                timeRemaining = 0;
+                roundOver = true;
+                // Handle end of round logic here
+            }
+        }
+    }
+}
